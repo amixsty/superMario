7db2a1493f0c602509796dc328a5cc1abee427f6
Amir Hossein Fattahi <amixsty@gmail.com>
2022-06-23 10:02:38 +0430

    new sence and enemys :)

---
+++ Assets/Script/flag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class flag : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D mario)
+    {
+        SceneManager.LoadScene("scene2");
+    }
+}
---
+++ Assets/Script/supermario.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SuperMario : MonoBehaviour
+{
+    private Rigidbody2D mario;
+    public float speed = 3;
+    public float jump = 6;
+    public int scores;
+    public GameObject bitcoin;
+    void Start()
+    {
+        mario = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float sp = Input.GetAxisRaw("Horizontal");
+        mario.velocity = new Vector2(sp*speed,mario.velocity.y);
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+            mario.velocity += Vector2.up*jump;
+    }
+    void  OnCollisionEnter2D(Collision2D bitcoin)
+    {
+        if (bitcoin.gameObject.name.StartsWith("bitcoin"))
+        {
+            Destroy (bitcoin.gameObject);
+            scores +=1;
+            Debug.Log(scores);
+        }
+    }
+}
